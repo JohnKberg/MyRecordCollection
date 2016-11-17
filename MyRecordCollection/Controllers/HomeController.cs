1f64793d56c7263171f1c132466d573e9f6bd2f5
JohnKberg <john.kberg81@gmail.com>
2016-11-17 17:23:36 +0100

    Made start page into Image gallery

--- MyRecordCollection/Controllers/HomeController.cs
+++ MyRecordCollection/Controllers/HomeController.cs
@@ -1,16 +1,30 @@
-﻿using System;
+﻿using MyRecordCollection.Models;
+using MyRecordCollection.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace MyRecordCollection.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            var albums = db.Albums.Include(a => a.Artist).Take(18).ToList();
+
+            HomePageViewModel vm = new HomePageViewModel() 
+            {
+                PageTitle = "Welcome to my record collection!",
+                PageSubTitle = "A mix of Punk rock, Punk rock and Punk rock",
+                Albums = albums
+            };
+
+            return View(vm);
         }
 
         public ActionResult About()
@@ -25,6 +39,15 @@ namespace MyRecordCollection.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ MyRecordCollection/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyRecordCollection.Models;
+
+namespace MyRecordCollection.ViewModels
+{
+    public class HomePageViewModel
+    {
+        public string PageTitle { get; set; }
+        public string PageSubTitle { get; set; }
+
+        public IEnumerable<Album> Albums { get; set; }
+    }
+}
